4f4fb939d0b7f22235e53ef02af822c065108748
Eric Crowell <eric.crowell@gdit.com>
2025-12-22 08:17:36 +0000

    test: add configuration for node environment tests

--- vite.config.ts
+++ vite.config.ts
@@ -22,26 +22,36 @@ export default defineConfig({
     allowedHosts: [ '.localhost' ],
   },
   test: {
-    projects: [ {
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-        storybookTest({
-          configDir: path.join(dirname, '.storybook'),
-        }) ],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [ {
-            browser: 'chromium',
-          } ],
+    projects: [
+      {
+        test: {
+          name: { label: 'node', color: 'green' },
+          environment: 'node',
+          include: [ 'nodejs/**/*.test.ts' ],
         },
-        setupFiles: [ '.storybook/vitest.setup.ts' ],
       },
-    } ],
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }) ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [ {
+              browser: 'chromium',
+            } ],
+          },
+          setupFiles: [ '.storybook/vitest.setup.ts' ],
+          include: [ 'nodejs/**/*.stories.ts' ],
+        },
+      },
+    ],
   },
 });
