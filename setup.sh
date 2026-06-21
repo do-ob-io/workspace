7f1e2862b046937005a449ff74010da3a94dcf84
eCrow <eric.crowell@ecrow.dev>
2026-06-21 05:44:55 +0000

    feat: add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Clone core projects
+git clone https://github.com/do-ob-io/core.git nodejs/core
+git clone https://github.com/do-ob-io/data.git nodejs/data
+git clone https://github.com/do-ob-io/hook.git nodejs/hook
+git clone https://github.com/do-ob-io/ui.git nodejs/ui
+
+# Install dependencies
+pnpm install
