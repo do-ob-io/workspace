b53a667e6f2d3c3646508aef95043c4c9521dcbb
eCrow <eric.crowell@ecrow.dev>
2026-06-21 05:49:03 +0000

    updated startup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Clone core projects
-git clone https://github.com/do-ob-io/core.git nodejs/core
-git clone https://github.com/do-ob-io/data.git nodejs/data
-git clone https://github.com/do-ob-io/hook.git nodejs/hook
-git clone https://github.com/do-ob-io/ui.git nodejs/ui
+git clone https://github.com/do-ob-io/core.git "${SCRIPT_DIR}/nodejs/core"
+git clone https://github.com/do-ob-io/data.git "${SCRIPT_DIR}/nodejs/data"
+git clone https://github.com/do-ob-io/hook.git "${SCRIPT_DIR}/nodejs/hook"
+git clone https://github.com/do-ob-io/ui.git "${SCRIPT_DIR}/nodejs/ui"
 
 # Install dependencies
-pnpm install
+pnpm install --prefix "${SCRIPT_DIR}"
