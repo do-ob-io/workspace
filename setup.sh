3b200501004bb1514c8130878f0bc83b0387c613
eCrow <eric.crowell@ecrow.dev>
2026-06-24 11:54:15 +0000

    feat: add git config to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set up user-based git ignore file
+git config --global core.excludesfile $HOME/.gitignore
+
 # Clone core projects
 git clone https://github.com/do-ob-io/core.git nodejs/core
 git clone https://github.com/do-ob-io/data.git nodejs/data
